920525b2e4c8daef812d9805e3ec159b2791bc5d
jli <jli@adrastea.ifi.uni-heidelberg.de>
2022-11-08 02:26:32 +0100

    update preprocess.

--- preprocess/SummaryMatch.py
+++ preprocess/SummaryMatch.py
@@ -1,76 +1,107 @@
 from collections import namedtuple
-from typing import List, NamedTuple
+from typing import NamedTuple
 import operator
 import heapq
 import numpy as np
 from rouge_score.rouge_scorer import _create_ngrams, _score_ngrams
-import split
+import spacy
+from .split import check_combine_str
+
+nlp = spacy.load("en_core_web_sm", disable=("ner")) # shouldn't disable lemma, tokenizer...
 
 
-def extract_similar_summaries(dataset, n: int=2, optimization_attribute: str = "recall"):
-    '''
+def extract_similar_summaries(
+    dataset, top_n: int = 3, match_n: int = 2, optimization_attribute: str = "fmeasure"
+):
+    """
     Extract top n sentences similar to summary sentences from reference.
-    '''
+    """
 
-    if dataset.features["source"]._type == "Value":  # One row of source is one article.
-        pass
+    dataset = check_combine_str(dataset, ["source", "target"])
 
-    elif (
-        dataset.features["source"]._type == "Sequence"
-    ):  # Lists with split sentences or paragraphs
-        if dataset.features["source"].feature._type == "Value":
-            dataset = split.combine_into_string(dataset) # combine list elements into a string
-        else:
-            raise TypeError("Unknown type of source and target!")
-
-    dataset = split.split_into_sentences(dataset)
-
-    match_n_col = np.ones(dataset.num_rows,dtype=int)*n
-    optimization_attribute_col = np.full(dataset.num_rows, optimization_attribute)
-  
-    dataset = dataset.add_column("match_n", match_n_col)
-    dataset = dataset.add_column("optimization_attribute", optimization_attribute_col)
-    dataset = dataset.map(map_top_rouges_n_match)
+    map_dict = {
+        "top_n": top_n,
+        "match_n": match_n,
+        "optimization_attribute": optimization_attribute,
+    }
+    dataset = dataset.map(map_top_rouges_n_match, fn_kwargs=map_dict)
 
     return dataset
 
 
-def top_rouges_n_match(summary: List[str], reference: List[str], n: int=2, optimization_attribute: str = "recall") -> NamedTuple:
-    '''
+def top_rouges_n_match(
+    summary: str,
+    reference: str,
+    top_n: int = 3,
+    match_n: int = 2,
+    optimization_attribute: str = "fmeasure",
+) -> NamedTuple:
+    """
     Based on summaries module
-    '''
+    """
 
-    similar_sentences = namedtuple("similar_sentences", "indices scores positions sentences")
+    # get doc format of summary and reference
+  
+    similar_sentences = namedtuple(
+        "similar_sentences", "indices scores positions sentences"
+    )
     similar_sentences.indices = []
     similar_sentences.scores = []
 
-    reference_ngrams = [_create_ngrams([token.lemma_ for token in sentence], n=n)
-                            for sentence in reference]
+    # combine n-grams and text sentence into one list comprehension loop
+    reference_zip = [
+        [_create_ngrams([token.lemma_ for token in sentence], n=match_n), sentence.text]
+        for sentence in nlp(reference).sents
+    ]
+    reference_zip = list(zip(*reference_zip))
+    reference_ngrams = reference_zip[0]
+    reference_list = reference_zip[1]
 
-    for sentence in summary:
-
-        target_ngrams = _create_ngrams([token.lemma_ for token in sentence], n=n)
-
-        score = [getattr(_score_ngrams(target_ngrams=target_ngrams, prediction_ngrams=source_ngrams), optimization_attribute) for _, source_ngrams in enumerate(reference_ngrams)]
+    for sentence in nlp(summary).sents:
         
+        target_ngrams = _create_ngrams([token.lemma_ for token in sentence], n=match_n)
+
+        score = [
+            getattr(
+                _score_ngrams(
+                    target_ngrams=target_ngrams, prediction_ngrams=source_ngrams
+                ),
+                optimization_attribute,
+            )
+            for _, source_ngrams in enumerate(reference_ngrams)
+        ]
+
         # use heapq to sort (for long lists)
-        topn = heapq.nlargest(3, enumerate(score), key=operator.itemgetter(1))
+        topn = heapq.nlargest(top_n, enumerate(score), key=operator.itemgetter(1))
         similar_sentences.indices += list(zip(*topn))[0]
         similar_sentences.scores += list(zip(*topn))[1]
 
     # sort and remove duplicate indices to make summaries consistent
-    sorted_scores = sorted(zip(set(similar_sentences.indices),similar_sentences.scores))
-    similar_sentences.indices = np.array(sorted_scores)[:,0].astype(int)
-    similar_sentences.scores = np.array(sorted_scores)[:,1]
+    sorted_scores = sorted(
+        zip(set(similar_sentences.indices), similar_sentences.scores)
+    )
+    similar_sentences.indices = np.array(sorted_scores)[:, 0].astype(int)
+    similar_sentences.scores = np.array(sorted_scores)[:, 1]
 
-    similar_sentences.sentences = list(np.array(reference)[similar_sentences.indices])
-    similar_sentences.positions = np.array(similar_sentences.indices) / max(len(reference) - 1, 1)
+    similar_sentences.sentences = list(
+        np.array(reference_list)[similar_sentences.indices]
+    )
+    similar_sentences.positions = np.array(similar_sentences.indices) / max(
+        len(reference_list) - 1, 1
+    )
 
     return similar_sentences
 
-def map_top_rouges_n_match(example):
 
-    similar_sentences = top_rouges_n_match(example["target"], example["source"], example["match_n"], example["optimization_attribute"])
+def map_top_rouges_n_match(example, top_n, match_n, optimization_attribute):
+
+    similar_sentences = top_rouges_n_match(
+        example["target"],
+        example["source"],
+        top_n,
+        match_n,
+        optimization_attribute,
+    )
 
     example["similar_summary"] = similar_sentences.sentences
     example["similar_summary_scores"] = similar_sentences.scores
--- preprocess/split.py
+++ preprocess/split.py
@@ -1,17 +1,50 @@
+from typing import List
 import spacy
 from datasets import load_dataset
 from spacy.language import Language
 
 
-def combine_into_string(dataset):
+def combine_into_string(dataset, feature: str):
     "Combine list into a string."
-    dataset = dataset.map(lambda example: {'source': ' '.join(example['source']), 'target': ' '.join(example['target'])})
+    dataset = dataset.map(lambda example: {feature: " ".join(example[feature])})
     return dataset
 
+
+def check_combine_feature(dataset, feature: str):
+    if dataset.features[feature]._type == "Value":  # One row of feature is one article.
+        pass
+
+    elif (
+        dataset.features[feature]._type == "Sequence"
+    ):  # Lists with split sentences or paragraphs
+        if dataset.features[feature].feature._type == "Value":
+            dataset = combine_into_string(
+                dataset, feature
+            )  # combine list elements into a string
+
+        else:
+            raise TypeError("Unknown type of feature and target!")
+
+    return dataset
+
+
+def check_combine_str(dataset, feature: List[str]):
+
+    for fe in feature:
+        dataset = check_combine_feature(dataset, fe)
+
+    return dataset
+
+
 def split_into_sentences(dataset):
-    '''
+    """
     Expected a string or 'Doc' as input.
-    '''
-    nlp = spacy.load('en_core_web_sm', disable=("tagger", "lemmatizer", "ner"))
-    dataset = dataset.map(lambda example: {'source': [str(sent) for sent in nlp(example['source']).sents], 'target': [str(sent) for sent in nlp(example['target']).sents]})
+    """
+    nlp = spacy.load("en_core_web_sm", disable=("tagger", "lemmatizer", "ner"))
+    dataset = dataset.map(
+        lambda example: {
+            "source": [str(sent) for sent in nlp(example["source"]).sents],
+            "target": [str(sent) for sent in nlp(example["target"]).sents],
+        }
+    )
     return dataset
