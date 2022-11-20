127be42c45e94027784a39c190ed65cacedad22d
Jiahui Li <jli@eirene.ifi.uni-heidelberg.de>
2022-11-20 18:35:28 +0100

    update train script - save intermediate summary samples in disk.

--- train/train_script_en.py
+++ train/train_script_en.py
@@ -3,23 +3,18 @@ import json
 import numpy as np
 import pandas as pd
 import os, sys
-
+from datasets import Dataset, load_dataset
 from LoadData import load_data, read_js
 from preprocess import SummaryMatch, GreedyAlign
 
-file_dir = os.path.dirname(os.path.dirname(__file__))
-
 def get_args():
-
-    # get dataset names
-    ds_names = read_js(file_dir + "/DataStats/ds_name_list.json")
 
     # get command arguments
     parser = argparse.ArgumentParser(
         description="Get training parameters of datasets"
     )
     parser.add_argument(
-        "-d", "--dataset", nargs=1, type=str, choices=ds_names, help="name of dataset"
+        "-d", "--dataset", nargs=1, type=str, help="name of dataset"
     )
     parser.add_argument("--split", nargs=1, type=str, help="split of dataset")
     parser.add_argument(
@@ -41,7 +36,19 @@ def get_args():
 
 if __name__ == '__main__':
 
+    base_path = "/home/jli/working_dir/2022-jiahui-li-thesis/data"
+
     args = get_args()
     dataset = load_data(args.dataset[0], args.split[0], args.sample_propor)
-    dataset_intermediate = SummaryMatch.extract_similar_summaries(dataset)
-    dataset_intermediate = GreedyAlign.extract_greedy_summaries(dataset)
+    try:
+        dataset_intermediate_top3 = Dataset.load_from_disk(base_path+args.dataset[0]+"_intermediate_top3")
+        dataset_intermediate_greedy = Dataset.load_from_disk(base_path+args.dataset[0]+"_intermediate_greedy")
+    except FileNotFoundError:
+        dataset_intermediate_top3 = SummaryMatch.extract_similar_summaries(dataset)
+        dataset_intermediate_greedy = GreedyAlign.extract_greedy_summaries(dataset)
+        dataset_intermediate_top3.save_to_disk(base_path+args.dataset[0]+"_intermediate_top3")
+        dataset_intermediate_greedy.save_to_disk(base_path+args.dataset[0]+"_intermediate_greedy")
+
+    
+
+
